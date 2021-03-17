7e8f3b85849c2c7a3b24db9cc124ec43e1eb5f41
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-03-17 20:33:22 +0100

    clean

--- models/Manager.php
+++ models/Manager.php
@@ -7,10 +7,8 @@ class Manager {
     if(is_null(self::$_pdo)) {
       self::$_pdo = 
         new PDO('mysql:host=localhost:8889;dbname=coffee;charset=utf8', 'root', 'root');
-
-      return self::$_pdo ;
-    } else {
-      return self::$_pdo;
-    }
+    } 
+    
+    return self::$_pdo ;
   }
 }
