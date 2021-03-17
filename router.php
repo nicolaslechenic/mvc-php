11b099c2bad86e4599a242cada56b5437ce2bb9d
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-03-17 20:35:10 +0100

    clean

--- router.php
+++ router.php
@@ -4,14 +4,4 @@ require("Controller.php");
 
 $action = $_GET["action"] ?? "home"; 
 
-switch($action) {
-  case "waiters":
-    waiters();
-    break;
-  case "edibles":
-    edibles();
-    break;
-  default:
-    home();
-    break;
-}
+$action();
